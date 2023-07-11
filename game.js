b1a55dfbb68a77830e508b9dcf9d771e10e34607
alanduartee <113642095+alanduartee@users.noreply.github.com>
2023-07-11 15:00:20 -0400

    casi final

--- game.js
+++ game.js
@@ -5,7 +5,9 @@ const ctx = canvas.getContext("2d");
 const gameContainer = document.getElementById('game-container');
 
 const flappyImg = new Image();
-flappyImg.src = 'assets/sonic2.png';
+flappyImg.src = 'assets/flappy_dunk.png';
+
+
 
 //Game constants
 const FLAP_SPEED = -3; //Improves the fluidity and accuracy of the bird when moving around the screen
